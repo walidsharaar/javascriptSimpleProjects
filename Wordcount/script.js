51f436fe9222027e91553e49c08f8c6107a3dd99
Sharaar <walidsharar@gmail.com>
2021-05-07 15:26:18 +0200

    files updated

---
+++ Wordcount/script.js
@@ -0,0 +1,12 @@
+var print = function(msg) {
+    
+    document.getElementById("output").innerHTML = "Length is " + msg;
+      
+  }
+      
+  document.getElementById("btn").onclick = function(event){
+  
+      print(document.getElementById('str').value.length);
+  }
+     
+  
